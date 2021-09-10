b353286c3aa63b797d06d832337e543c40530588
rahul8002 <rahulranjankua@gmail.com>
2021-09-10 23:36:52 +0530

    first commit

---
+++ ppmtool/src/main/java/io/agileIntelligence/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,14 @@
+package io.agileIntelligence.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
